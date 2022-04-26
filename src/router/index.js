86ec4503d0df6112d221a8b5f266d23e3dac28db
Gaston Martinez <gasttonbz@gmail.com>
2022-04-26 10:51:53 -0300

    Creacion del proyecto y definicion de las primeras rutas: GetIn, User y Admin

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import GetIn from '../views/GetIn.vue'
+import AdminView from '../views/AdminView.vue'
+import UserView from '../views/UserView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/getIn',
+    name: 'getIn',
+    component: GetIn
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/admin',
+    name: 'admin',
+    component: AdminView
+  },
+  {
+    path: '/user',
+    name: 'user',
+    component: UserView
   }
 ]
 
